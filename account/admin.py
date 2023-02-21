2e5336c2f0736359782e7b5700a30e94a38a4e6b
gitone912 <96557682+gitone912@users.noreply.github.com>
2023-02-22 01:16:43 +0530

    three

--- account/admin.py
+++ account/admin.py
@@ -1,4 +1,8 @@
 from django.contrib import admin
 from .models import UserData
-# Register your models here.
-admin.site.register(UserData)
+
+class UserDataAdmin(admin.ModelAdmin):
+    list_display = ('full_name', 'company', 'contact_phone', 'country', 'communication')
+    search_fields = ('full_name', 'company', 'contact_phone', 'country', 'communication')
+
+admin.site.register(UserData, UserDataAdmin)
